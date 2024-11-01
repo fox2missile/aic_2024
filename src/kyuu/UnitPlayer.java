8474a9ef3d395f1498bc83586227bf024966766c
吸血鬼ちゃん <chankyuuketsuki@gmail.com>
2024-11-01 08:05:14 +0000

    kyuu v21.1: finally handle exception

--- src/kyuu/UnitPlayer.java
+++ src/kyuu/UnitPlayer.java
@@ -1,6 +1,7 @@
 package kyuu;
 
 import aic2024.user.UnitController;
+import kyuu.log.LoggerStandard;
 import kyuu.tasks.Task;
 
 public class UnitPlayer {
@@ -10,10 +11,19 @@ public class UnitPlayer {
 		C c = new C(uc);
 		/*enemy team*/
 		Task strat = uc.isStructure() ? new HeadquarterTask(c) : new AstronautTask(c);
+		LoggerStandard exceptionLogger = new LoggerStandard(uc);
 
 		while (true) {
 			c.loc = uc.getLocation();
-			strat.run();
+			try {
+				strat.run();
+			} catch (Exception e) {
+				exceptionLogger.log("Exception!!!");
+				if (c.DEBUG) {
+					// lol I don't care, be gone
+					throw e;
+				}
+			}
 			uc.yield(); //End of turn
 		}
 	}
