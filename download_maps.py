e844c5c21cdfda0bcc77d9e2fad465cf4cb0a6b3
吸血鬼ちゃん <chankyuuketsuki@gmail.com>
2024-11-01 08:05:14 +0000

    Basic infra

---
+++ download_maps.py
@@ -0,0 +1,28 @@
+import requests
+import os
+from pathlib import Path
+
+
+def main() -> None:
+    maps_dir = 'maps'
+
+    list_response = requests.get("https://www.coliseum.ai/api/tournaments/aic2023/maps")
+    list_response.raise_for_status()
+
+    for obj in list_response.json():
+        if not obj["visible"]:
+            continue
+
+        map_name = obj["name"]
+        print(f"Downloading {map_name}")
+
+        map_response = requests.get(f"https://www.coliseum.ai/api/tournaments/aic2023/maps/{map_name.lower()}/file")
+        map_response.raise_for_status()
+
+        map_file = os.path.join(maps_dir, f'{map_name}.txt')
+        with open(map_file, 'wb') as file:
+            file.write(map_response.content)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_worker.py
@@ -0,0 +1,87 @@
+import os
+import subprocess
+import time
+
+import redis
+
+from datetime import datetime
+
+from redis import lock
+
+from test_utils import *
+
+
+class TestWorker:
+    def __init__(self):
+        self.redis = redis.Redis(host='localhost', port=6379, db=0)
+        self.lock_queue = lock.Lock(self.redis, QUEUE_LOCK_KEY)
+        self.id = os.getpid()
+
+    def log(self, msg: str):
+        print('[{}] Worker #{}: {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self.id, msg))
+
+    def run_match(self, match: Match):
+        replay_file = str(match) + '.bc23'
+        replay_file = replay_file.replace(' ', '-').replace(':', '_')
+        self.log("replay file: " + replay_file)
+        result = subprocess.run([
+            'cmd', '/c', 'ant',
+            '-Dmap=' + match.map,
+            '-Dpackage1=' + match.player_1,
+            '-Dpackage2=' + match.player_2,
+            '-Dreplay=' + replay_file,
+            'run_no_setup'], stdout=subprocess.PIPE)
+        output = result.stdout.decode('utf-8').split('\n')
+        # print(output)
+        winner = None
+        side = None
+        reason = None
+        reason_find_str = 'WinCondition: '
+        for i in range(-1, -20, -1):
+            if 'Winner' in output[i]:
+                print(output[i].split())
+                _, _, winner, _, side = output[i].split()
+                side = side.replace('(', '').replace(')', '')
+            if reason_find_str in output[i]:
+                _, reason = output[i].split(reason_find_str)
+        result = MatchResult(match=match, winner=winner, winner_side=side, round_count=7, win_reason=reason)
+        self.log(str(result))
+        result_lock = lock.Lock(self.redis, get_result_lock_key(match.id))
+        try:
+            result_lock.acquire()
+            result_list_key = get_result_list_key(match.id)
+            raw_result_list = self.redis.get(result_list_key)
+            result_list = MatchResultList(repr_str=raw_result_list.decode()) if raw_result_list is not None \
+                else MatchResultList()
+            result_list.append(result)
+            self.redis.set(result_list_key, str(result_list))
+        finally:
+            if result_lock.owned():
+                result_lock.release()
+
+    def loop(self):
+        self.log("started")
+        while True:
+            try:
+                self.lock_queue.acquire()
+                matches_str = self.redis.get(QUEUE_KEY).decode()
+                match: Match | None = None
+                if len(matches_str) > 0:
+                    matches = MatchQueue(repr_str=matches_str)
+                    match = matches.dequeue()
+                    self.log("running match: {}".format(match))
+                    self.redis.set(QUEUE_KEY, str(matches))
+                self.lock_queue.release()
+                if match is not None:
+                    self.run_match(match)
+                time.sleep(1)
+            except KeyboardInterrupt:
+                self.log("stopped")
+                break
+            finally:
+                if self.lock_queue.owned():
+                    self.lock_queue.release()
+
+
+if __name__ == '__main__':
+    TestWorker().loop()
